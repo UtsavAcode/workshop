360e77c2ea971a04e8cb5bd611bcdc8fd9312611
utsav <utsavadhikari452@gmail.com>
2023-05-25 13:50:20 +0545

    late commit and update

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    
- 
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    
+document.getElementById("frm").addEventListener("bt",function(event){
+  event.preventDefault();
 
-    if (username === "admin" && password === "password") {
-      alert("Login successful!");
-      
-    } else {
-      alert("Invalid username or password. Please try again.");
-    }
-  });
-  
+  var name =  document.getElementById("t1").value;
+  var email =document.getElementById("t2").value;
+  var password = document.getElementById("p").value;
+
+  var message = "Entered the data:\nName:"+name+"\nEmail"+email+"\nPassword:"+password;
+  alert(message);
+});
